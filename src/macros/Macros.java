0d2878a3c9176b28b76554c1d8a3bbeb441edcef
Kelsey Hyde <kelseyhyde@gmail.com>
2016-04-14 16:31:40 +1200

    Small changes to console interface.

--- src/macros/Macros.java
+++ src/macros/Macros.java
@@ -1,13 +1,30 @@
 
 package macros;
 
+import java.util.Scanner;
+
 /**
  * @author kelseyhyde
  */
 public class Macros {
 
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
         
+        System.out.println("Welcome to this macro counter! Please enter your total calories: ");
+        int calories = in.nextInt();
+        
+        Macro macro = new Macro(calories);
+        
+        System.out.println("Enter your fat percentage: ");
+        int fat = in.nextInt();
+        System.out.println("Enter your carbs percentage: ");
+        int carbs = in.nextInt();
+        System.out.println("Enter your protein percentage: ");
+        int protein = in.nextInt();
+        
+        macro.setMacros(fat, carbs, protein);
+        System.out.println(macro.printMacros());
     }
     
 }
