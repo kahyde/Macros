34933c1e743d6e59cadac9a6333da6e5b1a8caa3
Kelsey Hyde <kelseyhyde@gmail.com>
2016-04-14 16:29:44 +1200

    Fixed error in macro calculation.

--- src/macros/Macro.java
+++ src/macros/Macro.java
@@ -26,9 +26,9 @@ class Macro {
     }
     
     public void setMacros( int fat, int carbs, int protein){
-        this.fat = (calories / fat) * 100;
-        this.carbs = (calories / carbs) * 100;
-        this.protein = (calories / protein) * 100;
+        this.fat = (calories * fat) / 100;
+        this.carbs = (calories * carbs) / 100;
+        this.protein = (calories * protein) / 100;
     }
     
     public String printMacros(){
