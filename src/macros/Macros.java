4c07ffb1e58a5ec81cc2e7bdd4bb36f8d2c52f32
Kelsey Hyde <kelseyhyde@gmail.com>
2016-04-14 16:03:57 +1200

    Initial Commit

---
+++ src/macros/Macros.java
@@ -0,0 +1,13 @@
+
+package macros;
+
+/**
+ * @author kelseyhyde
+ */
+public class Macros {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
